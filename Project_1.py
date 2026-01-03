7a62aa952f783b89305135f1c7644587cc188fe7
Daksh choudhary <dakshchoudhary8881@gmail.com>
2026-01-04 01:35:38 +0530

    Grretings giver

--- Project_1.py
+++ Project_1.py
@@ -4,10 +4,14 @@ My first project of python
 project name -  Basic Calculator (only can do + , - , * , / )
 '''
 
-number1 = float(input("Enter first number "))
-number2 = float(input("Enter second number "))
+number1 = float(input("Enter first number: "))
+number2 = float(input("Enter second number: "))
 
-print("The addition of ",number1,"+",number2,"is",number1 + number2)
-print("The subtraction of ",number1,"-",number2,"is",number1 - number2)
-print("The multiplication of ",number1,"+",number2,"is",number1 * number2)
-print("The division of ",number1,"+",number2,"is",number1 / number2)
+print("Addition:", number1, "+", number2, "=", number1 + number2)
+print("Subtraction:", number1, "-", number2, "=", number1 - number2)
+print("Multiplication:", number1, "*", number2, "=", number1 * number2)
+
+if number2 != 0:
+    print("Division:", number1, "/", number2, "=", number1 / number2)
+else:
+    print("Division is not possible (division by zero)")
