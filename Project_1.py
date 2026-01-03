94d4290de79e87f6a7ec6c4056048e2417f071d8
Daksh choudhary <dakshchoudhary8881@gmail.com>
2026-01-03 23:06:36 +0530

    Update calculator project

---
+++ Project_1.py
@@ -0,0 +1,12 @@
+'''
+My first project of python 
+project name -  Basic Calculator (only can do + , - , * , / )
+'''
+
+number1 = float(input("Enter first number "))
+number2 = float(input("Enter second number "))
+
+print("The addition of ",number1,"+",number2,"is",number1 + number2)
+print("The subtraction of ",number1,"-",number2,"is",number1 - number2)
+print("The multiplication of ",number1,"+",number2,"is",number1 * number2)
+print("The divie of ",number1,"+",number2,"is",number1 / number2)
