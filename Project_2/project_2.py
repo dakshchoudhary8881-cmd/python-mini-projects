f053bce3de950f28aa2e000ce004504636857d68
Daksh choudhary <dakshchoudhary8881@gmail.com>
2026-01-04 01:41:49 +0530

    changed folder

---
+++ Project_2/project_2.py
@@ -0,0 +1,20 @@
+# ***************** auto  greets (good morning , afternoon , evening ) according to indian time ****************
+
+
+import time
+
+current_time = time.strftime("%H:%M:%S")
+print(current_time)
+
+#only print hours of the time
+hour = int(time.strftime("%H"))
+
+if(hour > 4 and hour < 12):
+    print("Good Morning Daksh")
+elif(hour > 12 and hour < 17 ):
+    print("Good Afternoon Daksh")
+elif(hour >= 17 and hour < 21):
+    print("Good Evening Daksh")
+else:
+    print("Good Night Daksh")
+
